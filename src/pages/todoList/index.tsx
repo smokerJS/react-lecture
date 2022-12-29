73ed79732347c0a7b4a84a16f4052177cc028261
정진솔 <smokerjs@woowahan.com>
2022-12-29 21:13:12 +0900

    주석추가

--- src/pages/todoList/index.tsx
+++ src/pages/todoList/index.tsx
@@ -3,6 +3,36 @@ import Todo from "./types/Todo";
 import Items from "./Items";
 import Form from "./Form";
 
+/**
+    함수, 변수 네이밍 맞추기
+    변수명 줄이기 금지 e 금지 무조건 event 풀로 쓰기
+    모든 타입 선언은 interface 로 작성하도록 연습하기 (FC<여기에 작성하면 안돼요>)
+
+
+    다음숙제
+    끝말잇기 만들어오기
+ */
+// function solution(N: number) {
+//     const binary = N.toString(2);
+//     const firstBinaryNum = binary.indexOf("1") + 1;
+//     const binaryGap = binary.slice(firstBinaryNum, binary.lastIndexOf("1") + 1);
+//     const count = binaryGap.split("1").map((zero) => zero.length);
+//     return count ? Math.max(...count) : 0;
+
+//   const binary = N.toString(2);
+//   let result = 0;
+
+//   binary.split("1").forEach((str) => {
+//     str.length >= result && (result = str.length);
+//   });
+//   return result;
+
+//   1. 문제에서 원하는게 무엇인지 파악
+//   => 1과 1사이에 0 개수가 가장 큰것이 길이가 몇인가?
+//     => 1과 1사이 = split
+//     => 0 개수 = length
+// }
+
 function App() {
   const [items, setItems] = useState<Todo[]>([]);
 
